00e2a88d41fa1ccb8a8f88eb584eeaf21a64d046
unknown <ass_fran@hotmail.com>
2023-03-11 18:28:43 -0300

    added the @data and @noargscontructor to solve some problems derivated from the Tareas class

--- src/main/java/com/ccindor/challenge/Tareas.java
+++ src/main/java/com/ccindor/challenge/Tareas.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Tareas {
 
